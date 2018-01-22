1cefe0f3bb6f0f94b945851b00879b7f32e41158
Brian Howard <bhoward@depauw.edu>
2018-01-22 15:42:32 -0500

    Added binary search

---
+++ stainless/BinarySearch.scala
@@ -0,0 +1,24 @@
+import stainless.lang._
+
+object BinarySearch {
+  def isSorted(arr: Array[Int]): Boolean = {
+    forall((i: Int, j: Int) => (0 <= i && i < j && j < arr.length) ==> arr(i) <= arr(j))
+  }
+
+  def search(arr: Array[Int], x: Int, lo: Int, hi: Int): Boolean = {
+    require(0 <= lo && lo <= hi+1 && hi < arr.length && isSorted(arr))
+    decreases(hi - lo + 1)
+
+    if (lo <= hi) {
+      val i = lo + (hi - lo) / 2
+      val y = arr(i)
+      if (x == y) true
+      else if (x < y) search(arr, x, lo, i-1)
+      else search(arr, x, i+1, hi)
+    } else {
+      false
+    }
+  } ensuring { result =>
+    forall((i: Int) => (lo <= i && i <= hi) ==> (arr(i) == x ==> result))
+  }
+}
